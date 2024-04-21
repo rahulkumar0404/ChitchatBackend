1207d51f1e8764168b5c04d18fe2c22f2c8b11a5
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-21 18:46:19 +0530

    remove and update group members routes

--- routes/chat.js
+++ routes/chat.js
@@ -5,7 +5,12 @@ import {
   getMyChats,
   getMyGroups,
   addMembers,
+  removeMember,
+  updateAdmin,
+  leaveGroup,
+  sendAttachments,
 } from '../controllers/chat.js';
+import { attachmentsMulter } from '../middlewares/multer.js';
 const app = express.Router();
 
 app.use(isAuthenticated);
@@ -14,5 +19,12 @@ app.post('/groupchat', newGroupChat);
 app.get('/my', getMyChats);
 app.get('/my/groups', getMyGroups);
 app.put('/add/members', addMembers);
+app.put('/remove/members', removeMember);
+app.put('/:groupId/member/:userId', updateAdmin);
+app.delete('/leave/:id', leaveGroup);
+
+// send attachment
+
+app.post('/message', attachmentsMulter, sendAttachments);
 
 export default app;
