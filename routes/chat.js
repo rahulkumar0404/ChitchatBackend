77572a31524054cd2279cff3edc670d73bedca46
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-24 00:54:40 +0530

    chat routes done!

--- routes/chat.js
+++ routes/chat.js
@@ -1,30 +1,54 @@
 import express from 'express';
-import { isAuthenticated } from '../middlewares/auth.js';
 import {
-  newGroupChat,
+  addMembers,
+  deleteChat,
+  getChatDetails,
+  getMessages,
   getMyChats,
   getMyGroups,
-  addMembers,
-  removeMember,
-  updateAdmin,
   leaveGroup,
+  newGroupChat,
+  reNameGroup,
+  removeMember,
   sendAttachments,
+  updateAdmin,
 } from '../controllers/chat.js';
+import { isAuthenticated } from '../middlewares/auth.js';
 import { attachmentsMulter } from '../middlewares/multer.js';
+import {
+  addMemberValidator,
+  createGroupChatValidator,
+  removeMemberValidator,
+  renameGroupValidator,
+  updateAdminParamsValidator,
+  updateAdminValidator,
+  sendAttachmentsValidator,
+  chatIdValidator,
+} from '../validators/jsonValidator.js';
 const app = express.Router();
 
 app.use(isAuthenticated);
 
-app.post('/groupchat', newGroupChat);
+app.post('/groupchat', createGroupChatValidator, newGroupChat);
 app.get('/my', getMyChats);
 app.get('/my/groups', getMyGroups);
-app.put('/add/members', addMembers);
-app.put('/remove/members', removeMember);
-app.put('/:groupId/member/:userId', updateAdmin);
+app.put('/add/members', addMemberValidator, addMembers);
+app.put('/remove/members', removeMemberValidator, removeMember);
+app.put(
+  '/:groupId/member/:userId',
+  updateAdminParamsValidator,
+  updateAdminValidator,
+  updateAdmin
+);
 app.delete('/leave/:id', leaveGroup);
 
 // send attachment
 
-app.post('/message', attachmentsMulter, sendAttachments);
+app.post('/message', attachmentsMulter,sendAttachmentsValidator, sendAttachments);
+
+app.get('/message/:id',chatIdValidator, getMessages);
+app.get('/:id',chatIdValidator, getChatDetails);
+app.put('/:id', renameGroupValidator, reNameGroup);
+app.delete('/:id',chatIdValidator, deleteChat);
 
 export default app;
