f834cf134c8b4c9c2aab0a552d9a9debfd47e2e3
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-25 04:04:45 +0530

    user routes created

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,31 @@
 import bcrypt from 'bcrypt';
-import { User } from '../models/user.js';
-import { sendToken, cookieOptions } from '../utils/features.js';
-import { ErrorHandler } from '../utils/utility.js';
-import { tryCatch } from '../middlewares/error.js';
+import mongoose from 'mongoose';
+import axios from 'axios';
 import {
-  INVALID_SIGNUP_DETAILS,
-  INVALID_LOGIN_PASSWORD,
   INVALID_LOGIN_EMAIL,
-  INVALID_USER,
+  INVALID_LOGIN_PASSWORD,
+  INVALID_SIGNUP_DETAILS,
+  PREVIOUS_USER_EXIST,
   USER_NOT_FOUND,
 } from '../constants/ExceptionMessage.js';
+import { tryCatch } from '../middlewares/error.js';
+import { Chat } from '../models/chat.js';
+import { User } from '../models/user.js';
+import { Request } from '../models/request.js';
+import {
+  cookieOptions,
+  emitEvent,
+  sendToken,
+  generatePassword,
+} from '../utils/features.js';
+import { ErrorHandler } from '../utils/utility.js';
+import { NEW_REQUEST, REFETCH_CHATS } from '../constants/constants.js';
+import { sendMail } from '../utils/mail.js';
+import { generate } from 'generate-password';
+const registerUser = tryCatch(async (req, res, next) => {
+  const { firstName, lastName, email, password, bio, avatar } = req.body;
 
-export const registerUser = tryCatch(async (req, res, next) => {
-  const { firstName, lastName, email, password, userName, bio, avatar } =
-    req.body;
-
-  if (!firstName || !email || !password || !userName) {
+  if (!firstName || !email || !password) {
     return next(`${INVALID_SIGNUP_DETAILS}`);
   }
 
@@ -23,6 +33,14 @@ export const registerUser = tryCatch(async (req, res, next) => {
   if (!avatar) {
     (avatarData['public_id'] = 'aaaa'), (avatarData['url'] = 'sssss');
   } else avatarData = avatar;
+
+  const isUserExistWithEmail = await User.find({ email: email });
+
+  if (isUserExistWithEmail.length > 0) {
+    return next(new ErrorHandler(`${PREVIOUS_USER_EXIST}`, 400));
+  }
+
+  const userName = await getUsername(8);
   const user = await User.create({
     first_name: firstName,
     last_name: lastName,
@@ -32,15 +50,16 @@ export const registerUser = tryCatch(async (req, res, next) => {
     bio,
     avatar: avatarData,
   });
-
+  let userMessage = `<div>Your Username is <b> ${userName}</b>. You can login with this Username or Email </div>`;
+  await sendMail(email, firstName, userMessage);
   const token = sendToken(user);
   return res
     .status(201)
     .cookie('chitChat', token, cookieOptions)
-    .json({ success: true, message: 'User Created successfully' });
+    .json({ _id: user._id.toString(), message: 'User Created successfully' });
 });
 
-export const login = tryCatch(async (req, res, next) => {
+const login = tryCatch(async (req, res, next) => {
   const { userName: value, password } = req.body;
 
   const user = await User.findOne({
@@ -64,7 +83,7 @@ export const login = tryCatch(async (req, res, next) => {
     .json({ success: true, message: `Welcome Back ${user.first_name}` });
 });
 
-export const getMyProfile = tryCatch(async (req, res, next) => {
+const getMyProfile = tryCatch(async (req, res, next) => {
   const user = await User.findById(req.userId);
 
   if (!user) {
@@ -74,7 +93,7 @@ export const getMyProfile = tryCatch(async (req, res, next) => {
   return res.status(200).json({ success: true, user });
 });
 
-export const logout = tryCatch(async (req, res, next) => {
+const logout = tryCatch(async (req, res, next) => {
   let cookies = { ...cookieOptions, maxAge: 0 };
   return res
     .status(200)
@@ -82,10 +101,211 @@ export const logout = tryCatch(async (req, res, next) => {
     .json({ success: true, message: 'Logout successfully' });
 });
 
-export const searchUser = tryCatch(async (req, res, next) => {
-  const { firstName, lastName } = req.query;
+const searchUser = tryCatch(async (req, res, next) => {
+  const { firstName = '' } = req.query;
 
-  const users = await User.find({ first_name: firstName, last_name: lastName });
+  const myChats = await Chat.find({
+    $and: [
+      { group_chat: false },
+      {
+        members: {
+          $elemMatch: { user: new mongoose.Types.ObjectId(req.userId) },
+        },
+      },
+    ],
+  });
+  const allUsersFromMyChats = myChats
+    .map((chat) => chat.members.map((member) => member.user))
+    .flat();
 
-  return res.status(200).json({ success: true, users });
+  const allUserExceptMeAndFriends = await User.find({
+    _id: { $nin: allUsersFromMyChats },
+    first_name: { $regex: firstName, $options: 'i' },
+  });
+  const users = allUserExceptMeAndFriends.map(
+    ({ _id, avatar, first_name, last_name }) => ({
+      _id,
+      avatar: avatar.url,
+      name: `${first_name} ${last_name}`,
+    })
+  );
+  return res.status(200).json({ users });
 });
+
+const sendFriendRequest = tryCatch(async (req, res, next) => {
+  const { receiverId } = req.body;
+
+  const request = await Request.find({
+    $or: [
+      { sender: req.userId, receiver: receiverId },
+      { sender: receiverId, receiver: req.userId },
+    ],
+  });
+
+  if (request.length > 0) {
+    return next(new ErrorHandler('Request already send', 400));
+  }
+
+  const newRequest = await Request.create({
+    sender: req.userId,
+    receiver: receiverId,
+  });
+
+  emitEvent(req, NEW_REQUEST, [receiverId]);
+  return res.status(200).json({
+    _id: newRequest._id.toString(),
+    message: 'Request sent successfully',
+  });
+});
+
+const acceptFriendRequest = tryCatch(async (req, res, next) => {
+  const { senderId, accept } = req.body;
+
+  const request = await Request.findOne({ sender: senderId })
+    .populate('sender', 'first_name last_name')
+    .populate('receiver', 'first_name last_name');
+
+  if (!request) {
+    return next(new ErrorHandler('Request not found', 400));
+  }
+  console.log(request);
+  if (request.receiver._id.toString() !== req.userId.toString()) {
+    return next(
+      new ErrorHandler('You are not authorized to accept this request', 401)
+    );
+  }
+
+  if (request.status === 'accepted') {
+    return next(new ErrorHandler('User exist in your chat'));
+  }
+  if (!accept && request.status === 'pending') {
+    await request.deleteOne();
+
+    return res.status(200).json({
+      message: 'Request deleted successfully',
+    });
+  }
+  const members = [
+    { user: request.sender._id.toString() },
+    { user: request.receiver._id.toString() },
+  ];
+
+  const [message] = await Promise.all([
+    Chat.create({
+      members,
+      group_name: `${request.sender.first_name} - ${request.receiver.first_name}`,
+    }),
+    request.updateOne({ status: 'accepted' }),
+  ]);
+
+  emitEvent(req, REFETCH_CHATS, members);
+  return res.status(200).json({
+    message: 'Friend Request accepted',
+    senderId: request.sender._id,
+    message_id: message._id.toString(),
+  });
+});
+
+const getMyNotification = tryCatch(async (req, res, next) => {
+  const request = await Request.find({ receiver: req.userId }).populate({
+    path: 'sender',
+    select: 'first_name last_name avatar',
+  });
+
+  const allRequests = request.map(({ _id, sender }) => ({
+    _id,
+    sender: {
+      _id: sender._id,
+      name: sender.name,
+      avatar: sender.avatar.url,
+    },
+  }));
+
+  const result = allRequests.length > 0 ? allRequests : 'No Request exist';
+  return res.status(200).json({ result });
+});
+
+const forgetPassword = tryCatch(async (req, res, next) => {
+  const { userName, recaptchaValue } = req.body;
+  const recaptchaSecretKey = process.env.RECAPTCHA_SECRET_KEY;
+  const response = await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecretKey}&response=${recaptchaValue}`
+  );
+  console.log(response);
+  if (response.success) {
+    return next(new ErrorHandler('Captcha Verification failed'));
+  }
+  const user = await User.find({ username: userName });
+
+  if (!user) {
+    return next(new ErrorHandler('User not found', 400));
+  }
+  const password = await generatePassword(8);
+  let userMessage = `Your New Password is <b>${password}</b>.<br>You can login with this Password`;
+  // await user.updateOne({ password: password });
+
+  await sendMail(user.email, user.first_name, userMessage);
+  // const token = sendToken(user); cookie('chitChat', token, cookieOptions)
+  return res.status(200).json({
+    // _id: user._id.toString(),
+    message: 'New Password send successfully to your email',
+  });
+});
+
+const getMyFriends = tryCatch(async (req, res, next) => {
+  const chatId = req.query.chatId;
+
+  const chats = await Chat.find({
+    $and: [
+      {
+        members: { $elemMatch: { user: req.userId.toString() } },
+      },
+      { group_chat: false },
+    ],
+  }).populate('members.user', 'first_name last_name avatar');
+
+  const friends = chats.map(({ members }) => {
+    const otherUser = members.find(
+      (member) => member.user._id.toString() !== req.userId.toString()
+    );
+    return {
+      _id: otherUser._id.toString(),
+      name: `${otherUser.user.first_name} ${otherUser.user.last_name}`,
+      avatar: otherUser.user.avatar.url,
+    };
+  });
+
+  if (chatId) {
+    const userChats = await Chat.findById(chatId);
+    console.log(userChats)
+    const chatMembers = userChats.members.map(member => member.user.toString())
+    const availableFriends = friends.filter(
+      (friend) => !chatMembers.includes(friend._id)
+    );
+    return res.status(200).json({ availableFriends });
+  } else {
+    return res.status(200).json({ friends });
+  }
+});
+
+async function getUsername(length) {
+  const username = generate({ length: length, numbers: false });
+  const existingUser = await User.findOne({ username });
+  if (!existingUser) {
+    return username;
+  }
+  return getUsername(length);
+}
+
+export {
+  registerUser,
+  login,
+  getMyProfile,
+  logout,
+  searchUser,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getMyNotification,
+  forgetPassword,
+  getMyFriends,
+};
