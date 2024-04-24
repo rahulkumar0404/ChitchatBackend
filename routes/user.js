d3878121d649f250cc0efafdec11991329a8775e
Rahul@kumar <drkagarwal05@gmail.com>
2024-04-25 04:05:45 +0530

    user routes

--- routes/user.js
+++ routes/user.js
@@ -7,16 +7,40 @@ import {
   getMyProfile,
   logout,
   searchUser,
+  sendFriendRequest,
+  acceptFriendRequest,
+  getMyNotification,
+  forgetPassword,
+  getMyFriends,
 } from '../controllers/user.js';
 import { isAuthenticated } from '../middlewares/auth.js';
-app.post('/signup', singleImageAvatar, registerUser);
-app.post('/login', login);
-
+import {
+  registerUserValidator,
+  loginUserValidator,
+  receiverIdValidator,
+  acceptRequestValidator,
+} from '../validators/jsonValidator.js';
+app.post('/signup', singleImageAvatar, registerUserValidator, registerUser);
+app.post('/login', loginUserValidator, login);
+app.post('/forget-password', forgetPassword)
 // After user must be logged in
 app.get('/profile', isAuthenticated, getMyProfile);
 app.get('/logout', isAuthenticated, logout);
 app.get('/search', isAuthenticated, searchUser);
 
+app.post(
+  '/sendrequest',
+  isAuthenticated,
+  receiverIdValidator,
+  sendFriendRequest
+);
+app.post(
+  '/acceptrequest',
+  isAuthenticated,
+  acceptRequestValidator,
+  acceptFriendRequest
+);
+app.get('/notifications', getMyNotification);
 
-
+app.get('/friends', isAuthenticated, getMyFriends)
 export default app;
