3b10ef673cb4eb9a9d46764d85560f841be60b44
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-03 00:04:43 +0530

    backend constants config

---
+++ constants/config.js
@@ -0,0 +1,6 @@
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:4173'],
+  credentials: true,
+};
+const CHITCHAT_TOKEN = 'chitChat';
+export { corsOptions, CHITCHAT_TOKEN };
