91fc67b23cd3027496b546b2f4686691323f107a
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-03 00:02:23 +0530

    cloudinary config fix

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-import {app, server} from './app.js'
+import { app, server } from './app.js';
 import { connectDb } from './utils/features.js';
+import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
 dotenv.config({ path: './config/.env' });
 const PORT = process.env.PORT || 3000;
 
 connectDb(process.env.MONGO_URL);
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
