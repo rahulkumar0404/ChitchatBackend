50c2902b613ef7e07cc85ff8a6b9aa032fd9ee25
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-03 00:07:03 +0530

    socket authenticator and token from constants from

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,9 +5,10 @@ import {
   INVALID_USER,
   INVALID_ADMIN_USER,
 } from '../constants/ExceptionMessage.js';
-
+import { User } from '../models/user.js';
+import { CHITCHAT_TOKEN } from '../constants/config.js';
 export const isAuthenticated = tryCatch(async (req, res, next) => {
-  const token = req.cookies['chitChat'];
+  const token = req.cookies[CHITCHAT_TOKEN];
 
   if (!token) {
     return next(new ErrorHandler(`${INVALID_USER}`, 403));
@@ -32,3 +33,20 @@ export const adminAuthenticated = tryCatch(async (req, res, next) => {
 
   next();
 });
+
+export const socketAuthenticator = async (err, socket, next) => {
+  try {
+    if (err) return next(err);
+    const authToken = socket.request.cookies[CHITCHAT_TOKEN];
+    if (!authToken) {
+      return next(new ErrorHandler(`${INVALID_USER}`, 403));
+    }
+    const decodedData = jwt.verify(authToken, process.env.SECRET_TOKEN);
+    const user = await User.findById(decodedData.id);
+    if (!user) return next(new ErrorHandler(`${INVALID_USER}`, 403));
+    socket.user = user;
+    return next();
+  } catch (err) {
+    return next(new ErrorHandler(err.message, 500));
+  }
+};
