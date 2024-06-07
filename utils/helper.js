7649b5017bbd458847590f185bd341ad56c4573a
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-07 21:47:21 +0530

    getBase64 and getMemberIdsFromMember function added to helper file

--- utils/helper.js
+++ utils/helper.js
@@ -4,3 +4,11 @@ export const getSockets = (users = []) => {
   const sockets = users.map((user) => userSocketIds.get(user.toString()));
   return sockets;
 };
+
+export const getBase64 = (file) =>
+  `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
+
+export const getMemberIdsFromMember = async (members) => {
+  const memberIds = members.map((member) => member.user.toString());
+  return memberIds;
+};
