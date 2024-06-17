e65fdddf9aaae487e0d41b129b88c71c45266cf2
Rahul@kumar <drkagarwal05@gmail.com>
2024-06-18 01:00:21 +0530

    onlineuser is handled in socket

--- socket.js
+++ socket.js
@@ -1,7 +1,10 @@
 import { v4 as uuid } from 'uuid';
 import {
+  CHAT_JOINED,
+  CHAT_LEAVED,
   NEW_MESSAGE,
   NEW_MESSAGE_ALERT,
+  ONLINE_USERS,
   START_TYPING,
   STOP_TYPING,
 } from './constants/events.js';
@@ -10,7 +13,7 @@ import { Message } from './models/message.js';
 import cookieParser from 'cookie-parser';
 import { socketAuthenticator } from './middlewares/auth.js';
 const userSocketIds = new Map();
-
+const onlineUsers = new Set();
 const setUpSocket = (io) => {
   io.use((socket, next) => {
     cookieParser()(socket.request, socket.request.res, async (error) => {
@@ -50,7 +53,7 @@ const setUpSocket = (io) => {
       try {
         await Message.create(messageForDatabase);
       } catch (error) {
-        console.log(error.message);
+        throw new Error(error.message)
       }
     });
 
@@ -66,8 +69,27 @@ const setUpSocket = (io) => {
       socket.to(membersSocket).emit(STOP_TYPING, { chatId });
     });
 
-    socket.on('disconnected', () => {
+    socket.on(CHAT_JOINED, async ({ userId, members }) => {
+      onlineUsers.add(userId);
+      if (members) {
+        const memberIds = await getMemberIdsFromMember(members);
+        const membersSocket = getSockets(memberIds);
+        io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+      }
+    });
+    socket.on(CHAT_LEAVED, async ({ userId, members }) => {
+      onlineUsers.delete(userId);
+      if (members) {
+        const memberIds = await getMemberIdsFromMember(members);
+        const membersSocket = getSockets(memberIds);
+        io.to(membersSocket).emit(ONLINE_USERS, Array.from(onlineUsers));
+      }
+    });
+
+    socket.on('disconnect', () => {
       userSocketIds.delete(user._id.toString());
+      onlineUsers.delete(user._id.toString());
+      socket.broadcast.emit(ONLINE_USERS, Array.from(onlineUsers));
     });
   });
 };
